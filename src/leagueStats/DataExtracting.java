1e37c48da7c62499a134ae16fee11c033dae1779
EmileFontanet <emile.fontanet@infomaniak.ch>
2017-12-29 19:44:24 +0100

    addAllEntries by trio/quadra/penta added

--- src/leagueStats/DataExtracting.java
+++ src/leagueStats/DataExtracting.java
@@ -1,6 +1,7 @@
 package leagueStats;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.Arrays;
 import org.json.JSONArray;
 import java.sql.Timestamp;
 import org.json.JSONException;
@@ -56,7 +57,6 @@ public class DataExtracting {
 
 		return winningTeam;
 	}
-	
 	public static ArrayList<Integer> getLosingTeam(JSONObject game){
 		ArrayList<Integer> losingTeam = new ArrayList<Integer>();
 		try {
@@ -75,5 +75,64 @@ public class DataExtracting {
 
 		return losingTeam;
 	}
-
+	public static void addAllCompEntriesTrio(ArrayList<Integer> completeComp, HashMap<ArrayList<Integer>, Integer> destination) {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				for(int k = 0; k< 5; k++) {
+					if(i<j && j<k) {
+						ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(completeComp.get(i), completeComp.get(j), completeComp.get(k)));
+						temp.sort(null);
+						if(destination.containsKey(temp)) {
+							destination.put(temp, destination.get(temp) +1);
+						}
+						else {
+							destination.put(temp, 1);
+						}
+					}
+				}
+			}
+		}
+	}
+	public static void addAllCompEntriesQuadra(ArrayList<Integer> completeComp, HashMap<ArrayList<Integer>, Integer> destination) {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				for(int k = 0; k< 5; k++) {
+					for(int l = 0; l<5; l++) {
+						if(i<j && j<k && k<l) {
+							ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(completeComp.get(i), completeComp.get(j), completeComp.get(k), completeComp.get(l)));
+							temp.sort(null);
+							if(destination.containsKey(temp)) {
+								destination.put(temp, destination.get(temp) +1);
+							}
+							else {
+								destination.put(temp, 1);
+							}
+						}
+					}	
+				}
+			}
+		}
+	}
+	public static void addAllCompEntriesPenta(ArrayList<Integer> completeComp, HashMap<ArrayList<Integer>, Integer> destination) {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				for(int k = 0; k< 5; k++) {
+					for(int l = 0; l<5; l++) {
+						for(int m = 0; m < 5; m++) {
+							if(i<j && j<k && k<l && l<m) {
+								ArrayList<Integer> temp = new ArrayList<>(Arrays.asList(completeComp.get(i), completeComp.get(j), completeComp.get(k), completeComp.get(l), completeComp.get(m) ));
+								temp.sort(null);
+								if(destination.containsKey(temp)) {
+									destination.put(temp, destination.get(temp) +1);
+								}
+								else {
+									destination.put(temp, 1);
+								}
+							}
+						}	
+					}	
+				}
+			}
+		}
+	}
 }
--- src/leagueStats/main.java
+++ src/leagueStats/main.java
@@ -3,20 +3,31 @@ package leagueStats;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
-
+import java.util.HashMap;
 import java.sql.Array;
 import java.util.ArrayList;
 public class main {
 	public static void main(String [] args ) {
-		//JSONObject json = new JSONObject();
 		String key = "RGAPI-424c6995-9201-4e7c-9047-1b5ac512fb36";
 		JSONObject json = Requests.getGameByIdRaw(3473272153L, key);
 		ArrayList<Integer> winningTeam = DataExtracting.getWinningTeam(json);
 		ArrayList<Integer> losingTeam =  DataExtracting.getLosingTeam(json);
 		System.out.println("game brut : " + json);
-		System.out.println(winningTeam);
-		System.out.println(losingTeam);
-
+		HashMap<ArrayList<Integer>, Integer> trioWinning = new HashMap<ArrayList<Integer>, Integer>();
+		HashMap<ArrayList<Integer>, Integer> trioLosing = new HashMap<ArrayList<Integer>, Integer>();
+		HashMap<ArrayList<Integer>, Integer> quadraWinning = new HashMap<ArrayList<Integer>, Integer>();
+		HashMap<ArrayList<Integer>, Integer> quadraLosing = new HashMap<ArrayList<Integer>, Integer>();
+		HashMap<ArrayList<Integer>, Integer> pentaWinning = new HashMap<ArrayList<Integer>, Integer>();
+		HashMap<ArrayList<Integer>, Integer> pentaLosing = new HashMap<ArrayList<Integer>, Integer>();
+		DataExtracting.addAllCompEntriesTrio(winningTeam, trioWinning);
+		DataExtracting.addAllCompEntriesQuadra(winningTeam, quadraWinning);
+		DataExtracting.addAllCompEntriesPenta(winningTeam, pentaWinning);
+		System.out.println(trioWinning);
+		System.out.println(trioWinning.size());
+		System.out.println(quadraWinning);
+		System.out.println(quadraWinning.size());
+		System.out.println(pentaWinning);
+		System.out.println(pentaWinning.size());
 		
 		
 	}
